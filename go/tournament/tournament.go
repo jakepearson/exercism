3b3a86465c8dc9a86ccb433551409c0cfaf71f4f
Jake Pearson <jake.pearson@ca.com>
2017-12-10 20:00:21 -0700

    Completed tournament

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -2,8 +2,10 @@ package tournament
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
+	"sort"
 	"strings"
 )
 
@@ -16,8 +18,12 @@ type Team struct {
 	Points int
 }
 
+func header() string {
+	return "Team                           | MP |  W |  D |  L |  P\n"
+}
+
 func (t Team) String() string {
-	return fmt.Sprintf("%s|%d|%d|%d|%d|%d")
+	return fmt.Sprintf("%-30s |%3d |%3d |%3d |%3d |%3d", t.Name, t.Count, t.Win, t.Draw, t.Loss, t.Points)
 }
 
 func getTeam(teams map[string]*Team, name string) *Team {
@@ -29,7 +35,7 @@ func getTeam(teams map[string]*Team, name string) *Team {
 	return teams[name]
 }
 
-func updateTeams(teams map[string]*Team, name1 string, name2 string, result string) {
+func updateTeams(teams map[string]*Team, name1 string, name2 string, result string) error {
 	team1 := getTeam(teams, name1)
 	team2 := getTeam(teams, name2)
 	team1.Count++
@@ -38,8 +44,8 @@ func updateTeams(teams map[string]*Team, name1 string, name2 string, result stri
 	case "win":
 		team1.Points += 3
 		team1.Win++
-		team1.Loss++
-	case "lose":
+		team2.Loss++
+	case "loss":
 		team2.Points += 3
 		team1.Loss++
 		team2.Win++
@@ -48,27 +54,67 @@ func updateTeams(teams map[string]*Team, name1 string, name2 string, result stri
 		team2.Points++
 		team1.Draw++
 		team2.Draw++
+	default:
+		return fmt.Errorf("Bad result name (%s)", result)
 	}
+	return nil
 }
 
-func parse(reader io.Reader) map[string]*Team {
+func toSlice(teams map[string]*Team) []*Team {
+	var result []*Team
+	for _, team := range teams {
+		result = append(result, team)
+	}
+	sort.Slice(result, func(i, j int) bool {
+		team1 := result[i]
+		team2 := result[j]
+		if team1.Points == team2.Points {
+			return team1.Name < team2.Name
+		}
+		return team1.Points > team2.Points
+	})
+	return result
+}
+
+func parse(reader io.Reader) ([]*Team, error) {
 	teams := map[string]*Team{}
 	buffer := new(bytes.Buffer)
 	buffer.ReadFrom(reader)
 	input := strings.Trim(buffer.String(), "\n")
-	fmt.Printf("Input: %s\n", input)
-	for _, line := range strings.Split(input, "\n") {
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		if strings.HasPrefix(line, "# ") {
+			continue
+		}
+
+		if len(strings.Trim(line, " \n")) == 0 {
+			continue
+		}
+
 		parts := strings.Split(line, ";")
-		fmt.Printf("Parts: %v\n", line)
-		updateTeams(teams, parts[0], parts[1], parts[2])
+		if len(parts) < 3 {
+			return nil, errors.New("Must have 2 semicolons per line")
+		}
+		error := updateTeams(teams, parts[0], parts[1], parts[2])
+		if error != nil {
+			return nil, error
+		}
 	}
-	return teams
+
+	return toSlice(teams), nil
 }
 
 func Tally(reader io.Reader, writer io.Writer) error {
-	teams := parse(reader)
+	teams, error := parse(reader)
+
+	if error != nil {
+		return error
+	}
+
 	output := new(bytes.Buffer)
 
+	output.WriteString(header())
 	for _, team := range teams {
 		output.WriteString(fmt.Sprintf("%s\n", team))
 	}
