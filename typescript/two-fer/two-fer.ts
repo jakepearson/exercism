65e6fa805f4ef3e94a7f114b8cca5bba2f372e8a
Jake Pearson <jake@Jakes-MacBook-Pro.local>
2022-09-27 15:23:37 -0600

    feat: two fer

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,19 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(name: string = "me"): string {
+  // ^                 ^   ^ this is called a return type; it's the type of the
+  // ^                 ^     value that is returned from this function
+  // ^                 ^
+  // ^                 parameters go here
+  // ^
+  // allows the tests to import this function and call it
+  // <-- Your code goes here. You may remove all the commentary in this file.
+
+  if(name === "me") {
+    return `One for you, one for ${name}.`
+  }
+  return `One for ${name}, one for me.`
+
+}
